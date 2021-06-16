3af61c774172f7765ed65e75711ccfb498f82f11
Julia <jgurina.exceedteam@gmail.com>
2021-06-16 16:52:04 +0300

    code fix

--- src/db/models/task/index.js
+++ src/db/models/task/index.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const costScheme = new Schema({
+const tasksScheme = new Schema({
   text: String,
   text2: String,
   text3: String,
 });
 
-module.exports = Cost = mongoose.model("cost", costScheme);
+module.exports = Cost = mongoose.model("cost", tasksScheme);
