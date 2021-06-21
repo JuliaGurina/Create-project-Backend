a3f5b36bef32960ae3b55e5c5cebce7ab62a3522
Julia <jgurina.exceedteam@gmail.com>
2021-06-21 10:17:03 +0300

    changing the name

--- src/db/models/task/index.js
+++ src/db/models/task/index.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const tasksScheme = new Schema({
-  text: String,
-  text2: String,
-  text3: String,
+  shop: String,
+  isDate: String,
+  price: String,
 });
 
 module.exports = Cost = mongoose.model("cost", tasksScheme);
